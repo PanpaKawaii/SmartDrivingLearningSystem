f7722976df2240fe67a2a1a4c9ce1830659fce56
PanpaKawaii <dngngchitriu2004@gmail.com>
2026-02-02 11:13:31 +0700

    Add Moving Label Input Component

---
+++ src/app/components/MovingLabelInput/MovingLabelInput.jsx
@@ -0,0 +1,31 @@
+import './MovingLabelInput.css';
+
+export default function MovingLabelInput({
+    type = 'text',
+    value,
+    onValueChange,
+    extraClassName = '',
+    extraStyle = {},
+    label = '',
+    labelStyle = '',
+    disable = false
+}) {
+    return (
+        <div className='moving-label-input-container' style={extraStyle}>
+            <input
+                type={type}
+                placeholder=''
+                value={value ?? ''}
+                onChange={(e) => {
+                    const val = type === 'number'
+                        ? Number(e.target.value)
+                        : e.target.value
+                    onValueChange({ value: val })
+                }}
+                className={`input ${extraClassName}`}
+                disabled={disable}
+            />
+            <label htmlFor={label} className={labelStyle}>{label}</label>
+        </div>
+    )
+}
--- src/app/pages/AdminPages/QuestionManagement/QuestionManagement.jsx
+++ src/app/pages/AdminPages/QuestionManagement/QuestionManagement.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from 'react';
+import MovingLabelInput from '../../../components/MovingLabelInput/MovingLabelInput';
 
 import './QuestionManagement.css';
 
 export default function QuestionManagement() {
+    const [text, setText] = useState('');
+
     return (
-        <div>QuestionManagement</div>
+        <div className='admin-container'>
+            <div className='inner-container management-container question-management-container'>
+
+                <MovingLabelInput
+                    type={'text'}
+                    value={text || ''}
+                    onValueChange={(propE) => setText(propE.value)}
+                    extraClassName={''}
+                    extraStyle={{}}
+                    label={'Name'}
+                    labelStyle={'left moving'}
+                />
+            </div>
+        </div>
     )
 }
